6ffb751b033d64751562e9cca15b1c8011e73350
Lou Pascreau <loupascreau@gmail.com>
2022-08-23 16:21:46 +0200

    login route added, axios depedencie added

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login">
+    <h1>This is the LOGIN page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from "vue";
+
+export default defineComponent({
+  name: "LoginView",
+});
+</script>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import axios from "axios";
+
+onMounted(() => {
+  axios
+    .get("http://localhost:3000/api/auth/42/login")
+    .then((response: { data: any }) => {
+      console.log("reponse : ", response.data);
+    })
+    .catch((error: any) => {
+      console.log(error);
+    });
+});
+</script>
