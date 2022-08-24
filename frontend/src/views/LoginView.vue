17811c1a6a00409d709d7f8b6657ec8e6e3ae90c
Lou Pascreau <loupascreau@gmail.com>
2022-08-24 17:00:35 +0200

    calling api auth from frontend is ok

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="login">
-    <h1>This is the LOGIN page</h1>
+    <img id="ping-pong" src="@/assets/pingPongIcon.png" />
+    <button>Sign in with 42</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, inject } from "vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "LoginView",
@@ -14,16 +15,41 @@ export default defineComponent({
 
 <script setup lang="ts">
 import { onMounted } from "vue";
-import axios from "axios";
 
 onMounted(() => {
-  axios
-    .get("http://localhost:3000/api/auth/42/login")
-    .then((response: { data: any }) => {
-      console.log("reponse : ", response.data);
-    })
-    .catch((error: any) => {
-      console.log(error);
-    });
+  // window.location.href = "http://localhost:3000/api/auth/42/login";
 });
 </script>
+
+<style>
+body {
+  background-color: #1e2a02;
+}
+
+.login {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 800px;
+}
+
+#ping-pong {
+  height: 200px;
+}
+
+button {
+  box-sizing: border-box;
+  padding: 19px 43px;
+  gap: 10px;
+  border-radius: 22px;
+  border: 5px solid;
+  /* border-image: linear-gradient(
+    254.5deg,
+    rgba(0, 95, 62, 1) -18.41%,
+    rgba(255, 218, 0, 1) 66.67%
+  ); */
+  font-size: 24px;
+  font-weight: bold;
+}
+</style>
