123b275bc9169b6e042adf5307f9671f7b59664f
Lou Pascreau <loupascreau@gmail.com>
2022-08-24 17:00:35 +0200

    calling api auth from frontend is ok

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: "http://localhost:8080",
+    methods: ['GET', 'PUT', 'POST', 'HEAD', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ 
     whitelist: true, 
     transform: true, 
