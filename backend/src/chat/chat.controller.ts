b6c7ac645ba5d428efa2ed5281d6fc5004b8f472
massiliab <massblri@gmail.com>
2022-08-29 18:23:02 +0200

    stock messages and authors

--- backend/src/chat/chat.controller.ts
+++ backend/src/chat/chat.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ChatService } from './chat.service';
-import { CreateChatDto } from './dto/create-chat.dto';
-import { UpdateChatDto } from './dto/update-chat.dto';
+import { Chat } from './entities/chat.entity';
+import { messageInfos } from './utils/types';
 
 @Controller('chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
+
+  @Post()
+  saveMessage(@Body() body: messageInfos): Promise<Chat> {
+    return this.chatService.saveMessage(body);
+  }
 }
--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
 import { ChatGateway } from './chat.gateway';
+import { Chat } from './entities/chat.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Chat])],
   controllers: [ChatController],
   providers: [ChatService, ChatGateway]
 })
