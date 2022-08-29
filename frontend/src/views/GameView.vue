c48119977f9c26af76f3ee8223724bfa08fd7314
Pacret <paco.bonilla.pro@gmail.com>
2022-08-29 19:05:03 +0200

    socket io added with test

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -1,27 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1>This is the Game page</h1>
+    <!-- <ul class="messages">
+      <li v-for="(message, index) in messages" :key="index" class="message">
+        {{ message.content }}
+      </li> -->
+    <!-- </ul> -->
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import socket from "../socket";
 
-export default defineComponent({
-  name: "GameView",
-  components: {
-    HelloWorld,
+export default {
+  data() {
+    return {
+      input: "",
+    };
   },
-});
+  methods: {},
+  computed: {},
+  created() {
+    socket.on("connect", function () {
+      console.log("socket connected");
+    });
+    socket.emit("send_message", function () {
+      console.log("front");
+    });
+  },
+};
 </script>
 
 <script lang="ts" setup>
-import { onBeforeMount } from "vue";
+import { Ref, ref, onBeforeMount } from "vue";
 import { useRouter } from "vue-router";
 
 const router = useRouter();
+const profile: Ref<any> = ref("");
 
 onBeforeMount(async () => {
   await fetch("http://localhost:3000/api/private", {
@@ -37,6 +52,7 @@ onBeforeMount(async () => {
       return response.json();
     })
     .then((user) => {
+      profile.value = user;
       console.log(user);
     })
     .catch((error) => {
