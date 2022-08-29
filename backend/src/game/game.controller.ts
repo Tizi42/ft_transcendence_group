f717635e8cfd6cde04539fbd888a0d533635b760
Pacret <paco.bonilla.pro@gmail.com>
2022-08-29 19:05:53 +0200

    socket io added with test (files missing with prev git add)

---
+++ backend/src/game/game.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { GameService } from './game.service';
+import { CreateGameDto } from './dto/create-game.dto';
+import { UpdateGameDto } from './dto/update-game.dto';
+
+@Controller('game')
+export class GameController {
+  constructor(private readonly gameService: GameService) {}
+}
