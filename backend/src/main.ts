e5f1b7ef2b609fb8d0f24a608f367fcaebb08b20
massiliab <massblri@gmail.com>
2022-08-29 16:23:16 +0200

    connection socket.io serveur client(front)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,18 @@
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.use(cookieParser());
+  app.enableCors({
+    origin: "http://localhost:8080",
+    methods: ['GET', 'PUT', 'POST', 'HEAD', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ 
     whitelist: true, 
     transform: true, 
