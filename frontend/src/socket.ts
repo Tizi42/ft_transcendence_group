7b104a08ba314bafe0d656808e1baf6a1de58b6c
Pacret <paco.bonilla.pro@gmail.com>
2022-08-31 17:50:28 +0200

    - login, search for a game, gameRoom creations.. - work around to play on the same computer (client socket its kinda the same on each page) - phaser added

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const URL = "http://localhost:3000";
 const socket = io(URL, { autoConnect: true });
 
-socket.onAny((event, ...args) => {
-  console.log(event, args);
-});
+// socket.onAny((event, ...args) => {
+//   console.log(event, args);
+// });
 export default socket;
