078b1f399c6c41572e2f2e1f4250d2bb6a9f7eaf
massiliab <massblri@gmail.com>
2022-08-31 11:09:38 +0200

    style

---
+++ backend/src/chat/entities/chatBox.entity.ts
@@ -0,0 +1,17 @@
+// import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn } from "typeorm";
+
+// @Entity({ name: 'chatbox' })
+// export class ChatBox {
+    
+//     @PrimaryGeneratedColumn()
+//     id: number;
+
+//     @Column()
+//     author: string;
+
+//     @Column()
+//     content: string;
+
+//     @CreateDateColumn()
+//     created_at: Date;
+// }
