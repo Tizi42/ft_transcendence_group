c2ebe2036256e8cb8d9cca867efb76f3424a563c
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-09-01 17:57:28 +0200

    setup sub-views in gameView

--- frontend/src/views/HistoryView.vue
+++ frontend/src/views/HistoryView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container">
-    <h1 class="p-3 text-center">Leaderboard</h1>
     <h1 class="p-3 text-center">Battle history</h1>
     <table v-if="dataReady" class="table table-striped table-bordered">
       <thead>
@@ -23,24 +22,30 @@
   </div>
 </template>
 
-<script setup>
-import { ref, onMounted } from "vue";
+<script lang="ts" setup>
+import { defineComponent, defineExpose } from "vue";
+import { Ref, ref, onMounted } from "vue";
 
-let battles = {};
+let battles: Ref<any> = ref("");
 let dataReady = false;
 
 function reloadData() {
-  battles = fetch("http://localhost:3000/api/battles/test", {
+  battles.value = fetch("http://localhost:3000/api/battles/test", {
     credentials: "include",
   });
   dataReady = true;
 }
 
-// lifecycle hooks
 onMounted(() => {
   reloadData();
   console.log("yo");
 });
+
+defineExpose(
+  defineComponent({
+    name: "HistoryView",
+  })
+);
 </script>
 
 <style>
---
+++ frontend/src/views/LeaderboardView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h1 class="p-3 text-center">Leaderboard</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LeaderboardView",
+});
+</script>
+
+<style>
+h1 {
+  color: white;
+}
+</style>
