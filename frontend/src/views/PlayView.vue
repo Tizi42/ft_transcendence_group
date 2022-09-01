d5be3dc81be9b655b2b5a2792950ee7810364b4e
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-09-01 21:28:58 +0200

    setup sub-views in game

--- frontend/src/views/PlayView.vue
+++ frontend/src/views/PlayView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1 class="p-3 text-center">Leaderboard</h1>
+    <h1 class="p-3 text-center">Play</h1>
   </div>
 </template>
 
@@ -15,5 +15,10 @@ export default defineComponent({
 <style>
 h1 {
   color: white;
+}
+
+nav a.router-link-active {
+  background-color: rgba(1, 71, 47, 1);
+  height: 200px;
 }
 </style>
