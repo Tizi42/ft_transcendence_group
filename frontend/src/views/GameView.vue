9eeeb8983de95f89b0116f873595e55df56870e8
Pacret <paco.bonilla.pro@gmail.com>
2022-09-01 17:57:35 +0200

    prototype gameplay

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="game">
     <div @click="initializeGame" class="flex">
-      <a href="#1" class="btn">Initialize</a>
+      <a class="btn">Initialize</a>
+    </div>
+    <div @click="searchGame" class="flex">
+      <a class="btn">searchGame</a>
     </div>
     <ion-phaser v-bind:game.prop="game" v-bind:initialize.prop="initialize" />
   </div>
@@ -10,10 +13,9 @@
 <script lang="ts">
 import Phaser from "phaser";
 import socket from "../socket";
-
-function test() {
-  console.log("PROUT");
-}
+import background from "../assets/game_assets/background.png";
+import padle from "../assets/game_assets/padle.png";
+import ball_image from "../assets/game_assets/ball.png";
 
 function tempCheck(to: string): boolean {
   return true;
@@ -27,43 +29,124 @@ socket.on("loged", (to: string) => {
   const socketId = socket.id;
   if (socketId === to) {
     console.log(socket.id + " : loged !");
-    socket.emit("queue_register", "normal");
   }
 });
 
-socket.on("game_found", (to: string) => {
-  const socketId = socket.id;
-  if (socketId === to) {
-    console.log(socket.id + " : game found !");
-  }
-});
+// socket.n("game_found", (to: string) => {
+//   const socketId = socket.id;
+//   if (socketId === to) {
+//     console.log(socket.id + " : game found !");
+//   }
+// });
 
 socket.emit("login");
+var ball;
+var l_paddle;
+var r_paddle;
+var isGameStarted = false;
 
 export default {
   data() {
     return {
       initialize: false,
       game: {
-        width: "60%",
-        height: "80%",
+        width: 1043,
+        height: 591,
         type: Phaser.AUTO,
+        physics: {
+          default: "arcade",
+          arcade: {
+            gravity: false,
+          },
+        },
         scene: {
+          preload() {
+            this.load.image("background", "background.png");
+            this.load.image("padle", "padle.png");
+            this.load.image("ball", "ball.png");
+          },
           init() {
             this.cameras.main.setBackgroundColor("#1E2B02");
           },
           create() {
-            test();
-            this.helloWorld = this.add.text(
-              this.cameras.main.centerX,
-              this.cameras.main.centerY,
-              "Hello World",
-              { font: "40px Arial", fill: "#ffffff" }
+            socket.on("game_found", (to: string) => {
+              const socketId = socket.id;
+              if (socketId === to) {
+                console.log(socket.id + " : game found !");
+              }
+            });
+            this.cursors = this.input.keyboard.createCursorKeys();
+            this.background = this.add.image(
+              this.cameras.main.width / 2,
+              this.cameras.main.height / 2,
+              "background"
             );
-            this.helloWorld.setOrigin(0.5);
+            var scaleX = this.cameras.main.width / this.background.width;
+            var scaleY = this.cameras.main.height / this.background.height;
+            var scale = Math.max(scaleX, scaleY);
+            this.background.setScale(scale);
+
+            l_paddle = this.physics.add.sprite(
+              this.cameras.main.width * 0.05,
+              this.cameras.main.height * 0.5,
+              "padle"
+            );
+            r_paddle = this.physics.add.sprite(
+              this.cameras.main.width * 0.95,
+              this.cameras.main.height * 0.5,
+              "padle"
+            );
+            ball = this.physics.add.sprite(
+              this.cameras.main.width * 0.5,
+              this.cameras.main.height * 0.5,
+              "ball"
+            );
+            ball.setCollideWorldBounds(true);
+            ball.setScale(0.8);
+            l_paddle.setCollideWorldBounds(true);
+            r_paddle.setCollideWorldBounds(true);
+            ball.setBounce(1, 1);
+            this.physics.add.collider(ball, l_paddle);
+            this.physics.add.collider(ball, r_paddle);
+            l_paddle.setImmovable(true);
+            r_paddle.setImmovable(true);
+            socket.emit("queue_register", "normal");
           },
           update() {
-            this.helloWorld.angle += 1;
+            if (!isGameStarted) {
+              const initialVelocityX = 100;
+              const initialVelocityY = 100;
+
+              ball.setVelocityX(initialVelocityX);
+              ball.setVelocityY(initialVelocityY);
+              isGameStarted = true;
+            }
+            if (ball.body.x < l_paddle.body.x) {
+              console.log("player 1 win");
+            }
+            if (ball.body.x > r_paddle.body.x) {
+              console.log("player 2 win");
+            }
+            if (this.cursors.up.isDown) {
+              l_paddle.setVelocityY(-100);
+            } else if (this.cursors.down.isDown) {
+              l_paddle.setVelocityY(100);
+            } else if (this.cursors.up.isUp) {
+              l_paddle.setVelocityY(0);
+            } else if (this.cursors.down.isUp) {
+              l_paddle.setVelocityY(0);
+            }
+
+            //else if (this.cursors.down.isDown) {
+            //   l_paddle.setVelocityY(-100);
+            // } else if (this.cursors.down.isUp) {
+            //   l_paddle.setVelocityY(0);
+            // } else {
+            //   l_paddle.setVelocityY(0);
+            // }
+            // if (dir) {
+            //   l_paddle.
+            // }
           },
         },
       },
@@ -71,6 +154,9 @@ export default {
   },
   methods: {
     initializeGame() {
+      console.log("test");
+    },
+    searchGame() {
       this.initialize = true;
     },
   },
