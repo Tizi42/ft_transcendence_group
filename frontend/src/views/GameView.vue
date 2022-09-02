83f137385f53d35b7ac83eb1dc5a4ff7b84eeb07
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-09-02 20:26:28 +0200

    menu game ok

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="logo" src="../assets/pingPongIcon.png" />
-    <router-link :to="{ name: 'play' }">
-      <button class="gameButton">Play</button>
+  <div class="menu">
+    <router-link :to="{ name: 'play' }" class="gameButtonPlay">
+      Play !
     </router-link>
-    <router-link :to="{ name: 'leaderboard' }">
-      <button class="gameButton">Leaderboard</button>
+    <router-link :to="{ name: 'play' }" class="gameButton">
+      Watch a game
     </router-link>
-    <router-link :to="{ name: 'history' }">
-      <button class="gameButton">Match history</button>
+    <router-link :to="{ name: 'leaderboard' }" class="gameButton">
+      Leaderboard
+    </router-link>
+    <router-link :to="{ name: 'history' }" class="gameButton">
+      Match history
     </router-link>
   </div>
 </template>
@@ -47,3 +49,39 @@ onBeforeMount(async () => {
     });
 });
 </script>
+
+<style scoped>
+.gameButton,
+.gameButtonPlay {
+  display: block;
+  background: none;
+  color: var(--main-color);
+  font-family: "Outfit SemiBold";
+  font-size: 30px;
+  margin: 0;
+  width: 354px;
+  padding: 15px;
+  text-decoration: none;
+  border: solid;
+  border: 5px solid var(--main-color);
+  border-radius: 53px;
+  margin-top: 30px;
+}
+
+.gameButtonPlay {
+  font-size: 40px;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  margin-bottom: 30px;
+  border: 8px solid var(--main-color);
+}
+
+.menu {
+  height: 100vh;
+  width: 100vw;
+  padding-top: 17%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
