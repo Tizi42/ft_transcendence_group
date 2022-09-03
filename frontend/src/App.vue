f747b0f6c4b58456ec04ba5af3656e7b8bf3ac32
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-09-03 13:48:41 +0200

    reorganized style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,17 +3,17 @@
     <nav id="navbar">
       <router-link to="/">
         <div class="link-container">
-          <img src="@/assets/icon-pingpong-stroke.png" alt="icon PingPong" />
+          <img src="@/assets/icons/pingpong.svg" alt="icon PingPong" />
         </div>
       </router-link>
       <router-link to="chat">
         <div class="link-container">
-          <img src="@/assets/icon-chat-stroke.png" alt="icon Chat" />
+          <img src="@/assets/icons/chat.svg" alt="icon Chat" />
         </div>
       </router-link>
       <router-link to="user">
         <div class="link-container">
-          <img src="@/assets/icon-user-stroke.png" alt="icon User" />
+          <img src="@/assets/icons/profile.svg" alt="icon User" />
         </div>
       </router-link>
     </nav>
