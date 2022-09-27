4eb746db029f7e51fb3fa6b963606d846db7a890
Lou Pascreau <loupascreau@gmail.com>
2022-09-27 14:20:55 +0200

    css chat ongoing

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log'],
+  });
   
   app.setGlobalPrefix('api');
   app.use(cookieParser());
