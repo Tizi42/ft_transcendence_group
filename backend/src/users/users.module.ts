d3bcc7543480a81b20df72fa594c6431c5e614b3
massiliab <massblri@gmail.com>
2022-09-27 19:17:10 +0200

    fixing

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Chat } from 'src/chat/entities/chat.entity';
 import { UsersController } from './users.controller';
 import { User } from './users.entity';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Chat])],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [
