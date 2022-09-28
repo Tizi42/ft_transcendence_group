0514953d1b130f5b8b5995bcf14f5992696c5535
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-09-28 19:30:12 +0200

    start game overlay, chat focus, emoji key listener

---
+++ frontend/src/components/Game/GameBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="gameBox">Game Box</div>
+</template>
+
+<script lang="ts" setup>
+import { defineComponent, defineExpose } from "vue";
+
+defineExpose(
+  defineComponent({
+    name: "GameBox",
+  })
+);
+</script>
+
+<style scoped>
+.gameBox {
+  width: 70%;
+  height: 65%;
+  background: #1e2b02;
+  border: 5px solid #ffcb00;
+  border-radius: 22px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+}
+</style>
---
+++ frontend/src/components/Game/OverlayTopBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="overlayTopBar">TopBar</div>
+</template>
+
+<script lang="ts" setup>
+import { defineComponent, defineExpose } from "vue";
+
+defineExpose(
+  defineComponent({
+    name: "OverlayTopBar",
+  })
+);
+</script>
+
+<style scoped>
+.overlayTopBar {
+  width: 90%;
+  height: 20%;
+}
+</style>
