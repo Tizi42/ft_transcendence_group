ce3ccf16460615a7bd390402bbd3f75690efd58a
Lou Pascreau <loupascreau@gmail.com>
2022-09-28 18:08:58 +0200

    fix conflits chat view

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -1,4 +1,78 @@
 <template>
+  <div class="chat">
+    <div class="container-channels">
+      <div class="nav-channels">
+        <button
+          id="players-col"
+          :class="{ selected: isActive === 'players' }"
+          @click="select('players')"
+        >
+          Players
+        </button>
+        <button
+          id="channels-col"
+          :class="{ selected: isActive === 'channels' }"
+          @click="select('channels')"
+        >
+          Channels
+        </button>
+      </div>
+      <ul class="list-friends" v-if="isActive === 'players'">
+        <li
+          v-for="friend in user.friends"
+          :key="friend"
+          @click="renderCorresponding(friend)"
+        >
+          <div class="avatar-frame">
+            <img :src="friend.picture" />
+          </div>
+          <div class="friend-frame">
+            <div v-if="friend.status === 'offline'" class="red-point"></div>
+            <div v-if="friend.status === 'online'" class="green-point"></div>
+            <h3>{{ friend.username }}</h3>
+            <!-- <p>{{ lastMessage[profile] }}</p> -->
+          </div>
+        </li>
+      </ul>
+      <ul class="list-channels" v-else>
+        <li>channel</li>
+      </ul>
+    </div>
+    <!-- <MessagesView :chosenProfile="chosenProfile"></MessagesView> -->
+    <div class="container-chat">
+      <div class="container-messages">
+        <div v-for="message in history" :key="message">
+          <div
+            class="messages"
+            id="from-others"
+            v-if="message.authorId != user.id"
+          >
+            <img :src="message.dest.picture" />
+            <p>{{ message.content }}</p>
+          </div>
+          <div class="messages" id="from-user" v-else>
+            <img :src="message.author.picture" />
+            <p>{{ message.content }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="message-input">
+        <form @submit.prevent="onSubmit">
+          <input
+            v-model="messageText"
+            type="text"
+            placeholder="Your message.."
+            class="message-text"
+          />
+          <button type="submit">
+            <img src="@/assets/icons/send-icon.png" alt="send-message" />
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+<!-- <template>
   <div class="chatPage">
     <h1>Start chating</h1>
     <div class="chatroom">
@@ -10,17 +84,17 @@
         <input type="text" placeholder="Search.." />
         <div
           class="block"
-          v-for="(prof, index) in profileFrom"
-          :key="index"
-          @click="renderCorresponding(prof)"
+          v-for="profile in profileFrom"
+          :key="profile"
+          @click="renderCorresponding(profile)"
         >
-          <img :src="prof.picture" class="photo select" />
+          <img :src="profile.picture" class="photo select" />
           <div class="infos">
             <p id="userinfos">
-              {{ prof.username }}
+              {{ profile.username }}
             </p>
             <p id="messagelist">
-              {{ lastMessage[index] }}
+              {{ lastMessage[profile] }}
             </p>
           </div>
         </div>
@@ -28,7 +102,7 @@
       <MessagesView :chosenProfile="chosenProfile"></MessagesView>
     </div>
   </div>
-</template>
+</template> -->
 
 <script lang="ts" setup>
 import { Ref, ref, onBeforeMount, onUnmounted, onMounted } from "vue";
@@ -36,27 +110,64 @@ import socket from "@/socket";
 import MessagesView from "../components/MessagesView.vue";
 import { getUrlOf } from "@/router";
 import { useUserStore } from "@/stores/user";
+import "@/assets/styles/chat.css";
 
 const lastMessage: Ref<any> = ref([]);
 const profile: Ref<any> = ref("");
 const profileFrom: Ref<Array<any>> = ref([]);
 const chosenProfile: Ref<any> = ref("");
+const user: any = useUserStore();
+const isActive: Ref<string> = ref("players");
+const messageText: Ref<string> = ref("");
+const history: Ref<any> = ref([]);
+
+const select = (id: string) => {
+  isActive.value = id;
+};
+
+function onSubmit() {
+  const data = {
+    content: messageText.value,
+    author: user.id,
+    dest: 1,
+  };
+  console.log("dest =", data.dest);
+  socket.emit("send_message", data);
+  messageText.value = "";
+  window.location.reload();
+}
+
+function getMessages() {
+  console.log("profile = ", 1);
+  fetch(getUrlOf("api/chat/messages/" + 1))
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((el: any) => {
+        console.log("history = ", el);
+        history.value.push(el);
+      });
+    })
+    .catch((err) => console.error(err));
+}
 
 onBeforeMount(async () => {
-  const user = useUserStore();
-  fetch(getUrlOf("api/chat/dest"))
-    .then((response) => response.json())
+  await fetch(getUrlOf("api/chat/dest"))
+    .then((response) => {
+      return response.json();
+    })
     .then((data) => {
       getAllDest(data);
     })
-    .catch((err) => console.error(err));
+    .catch((err) => {
+      console.error(err);
+    });
+  user.doFetchFriends();
+  getMessages();
 });
 
-function getAllDest(response: any[]) {
-  let dest: any[];
-  dest = response;
+function getAllDest(dest: any[]) {
   for (let i = 0; i < dest.length; i++) {
-    if (dest[i].dest.id !== 1) {
+    if (dest[i].dest.id !== user.id) {
       profileFrom.value.push(dest[i].dest);
       getLastMessage(dest[i].dest.id);
     }
@@ -76,76 +187,3 @@ function renderCorresponding(prof: any) {
   console.log("after = ", chosenProfile.value);
 }
 </script>
-
-<style>
-.chatPage {
-  background: linear-gradient(
-    116.6deg,
-    #005f3e -20.9%,
-    #feca00 99.99%,
-    #ffda00 100%
-  );
-  height: 100vh;
-  width: 100vw;
-  padding-top: 2em;
-  padding-bottom: 5em;
-  display: flex;
-  flex-direction: column;
-}
-.chatroom {
-  display: flex;
-  flex-direction: row;
-  height: 80%;
-  width: 100%;
-}
-.box {
-  overflow-y: scroll;
-  scroll-behavior: smooth;
-  background: #1e2b02;
-  border-radius: 22px;
-  overflow: auto;
-  box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.3);
-  height: 100%;
-  margin-right: 1%;
-}
-.onglets {
-  display: flex;
-  flex-direction: row;
-}
-.list {
-  width: 15%;
-  margin-left: 5%;
-}
-.photo {
-  display: block;
-  width: 35px;
-  height: 35px;
-  border-radius: 80px;
-  margin-top: 2%;
-  margin-left: 2%;
-  box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.3);
-}
-.block {
-  cursor: pointer;
-  background: #005f3e;
-  box-shadow: 2px 2px 2px 2px rgba(1, 53, 28, 0.527);
-  display: flex;
-  flex-direction: row;
-}
-.block:active {
-  background-color: #feca00;
-}
-#messagelist {
-  margin-left: 5%;
-  font-size: 10px;
-}
-.infos {
-  width: 50%;
-}
-#userinfos {
-  font-family: "Outfit SemiBold";
-  font-size: 10px;
-  color: white;
-  padding-right: 40%;
-}
-</style>
