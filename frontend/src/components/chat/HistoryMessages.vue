56ebade9a32a3869785e5e0c15484d49f59497c7
Lou PASCREAU <loupascreau@gmail.com>
2022-09-30 15:11:44 +0200

    components chat working, reste a recup les message quavec la personne concerner

---
+++ frontend/src/components/chat/HistoryMessages.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container-messages">
+    <div v-for="message in historyMessages" :key="message">
+      <div
+        class="messages"
+        id="from-others"
+        v-if="message.authorId != user.id"
+      >
+        <img :src="message.dest.picture" />
+        <p>{{ message.content }}</p>
+      </div>
+      <div class="messages" id="from-user" v-else>
+        <img :src="message.author.picture" />
+        <p>{{ message.content }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useUserStore } from '@/stores/user';
+import { defineComponent } from 'vue';
+
+const user: any = useUserStore();
+
+interface Props {
+  historyMessages: Array<any>;
+}
+
+defineProps<Props>();
+
+defineExpose(
+  defineComponent({
+    name: "HistoryMessages",
+  })
+);
+</script>
