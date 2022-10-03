191591ee6a83fd3b93366c242428fcb77766de80
Lou PASCREAU <loupascreau@gmail.com>
2022-10-03 18:03:19 +0200

    creation app gateway general pour separer le code du chat et du game

--- backend/src/gateway.ts
+++ backend/src/gateway.ts
@@ -1,6 +1,7 @@
-import { OnGatewayConnection, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
 import { ChatService } from "./chat/chat.service";
+import { UsersService } from "./users/users.service";
 
 @WebSocketGateway({
   cors: {
@@ -9,15 +10,63 @@ import { ChatService } from "./chat/chat.service";
     credentials: true,
   },
 })
-export class AppGateway implements OnGatewayConnection {
+export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
   constructor(
-    chatService: ChatService,
+    readonly chatService: ChatService,
+    readonly usersService: UsersService,
   ) {}
 
+  afterInit(server: Server) {
+    console.log("initialized :", server);
+  }
+
   async handleConnection(socket: Socket) {
-    console.log("socket io id in app = ", socket.id);
+    const users = [];
+
+    socket.data = await this.chatService.getUserFromSocket(socket);
+    if (socket.data) {
+      this.usersService.updateIsOnline(socket.data.id, "online");
+    }
+
+    // console log all socket detected, even login or not //
+    for (let [id, socket] of this.server.of("/").sockets) {
+      users.push({
+        userID: id,
+        username: socket.data?.username,
+      });
+    }
+    console.log("users = ", users);
+    // ////////////////////////////////////////////////// //
+  }
+
+  handleDisconnect(client: any) {
+    const users = [];
+    let   stillOnline: boolean = false;
+    if (client.data) {
+      console.log("disconnected :", client.data.username);
+      for (let [id, socket] of this.server.of("/").sockets) {
+        if (socket.data) {
+          if (socket.data.username === client.data.username) {
+            stillOnline = true;
+          }
+        }
+      }
+      if (!stillOnline) {
+        this.usersService.updateIsOnline(client.data.id, "offline");
+      }
+    }
+
+    // console log all socket detected, even login or not //
+    for (let [id, socket] of this.server.of("/").sockets) {
+      users.push({
+        userID: id,
+        username: socket.data?.username,
+      });
+    }
+    console.log("users = ", users);
+    // ////////////////////////////////////////////////// //
   }
 }
