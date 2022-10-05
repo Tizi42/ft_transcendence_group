740dca1126f2506ccf575709cfcbcbe6da2c37a7
Lou PASCREAU <loupascreau@gmail.com>
2022-10-05 17:47:11 +0200

    channels css ongoing

--- frontend/src/components/chat/ChannelsList.vue
+++ frontend/src/components/chat/ChannelsList.vue
@@ -1,11 +1,33 @@
 <template>
+  <div class="options-channel">
+    <form id="search-in-my-channels" @submit.prevent="onSubmit">
+      <div>
+        <input
+          id="input-search-channels"
+          type="text"
+          placeholder="Search..."
+          v-model="inputSearch"
+        />
+        <button type="submit">
+          <img src="@/assets/icons/search.svg" />
+        </button>
+      </div>
+    </form>
+    <img src="@/assets/icons/icon-add.png" alt="Create new channel" />
+  </div>
   <ul class="list-channels">
     <li>channel</li>
   </ul>
 </template>
 
 <script lang="ts" setup>
-import { defineComponent, defineExpose } from "vue";
+import { defineComponent, defineExpose, ref, Ref } from "vue";
+
+const inputSearch: Ref<string> = ref("");
+
+const onSubmit = () => {
+  console.log("inputSearch = ", inputSearch);
+};
 
 defineExpose(
   defineComponent({
