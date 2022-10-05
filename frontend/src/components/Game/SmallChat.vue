81164b72f87f79c7b82de299776838c5bd155773
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-10-05 16:35:31 +0200

    chat operational ingame

--- frontend/src/components/Game/SmallChat.vue
+++ frontend/src/components/Game/SmallChat.vue
@@ -5,6 +5,7 @@
       class="chatBar"
       v-model="message"
       placeholder="Chat here..."
+      maxlength="80"
       @focusin="chatting(true)"
       @focusout="chatting(false)"
       @submit="sendMsg"
