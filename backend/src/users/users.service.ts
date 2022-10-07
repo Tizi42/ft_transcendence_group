04d01e4dd96bb91ae9b5faacd423b0cad77bf980
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-10-07 15:12:58 +0200

    dont create fake user if already 10 in the database + remove invalid credentials error

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -87,6 +87,10 @@ export class UsersService {
     await this.usersRepository.delete(id);
   }
 
+  async needRecreate(): Promise<boolean> {
+	return await this.usersRepository.count() < 10;
+  }
+
   async removeAll(): Promise<void> {
     await this.chatRepository.delete({});
     await this.usersRepository.delete({});
