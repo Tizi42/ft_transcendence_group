b71661788b35155d65df6093e8c7307ada12a599
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-10-12 14:40:50 +0200

    fix add friend modal

--- frontend/src/components/users/UserFriends/MyModal.vue
+++ frontend/src/components/users/UserFriends/MyModal.vue
@@ -26,22 +26,37 @@ defineExpose(
 
 <style scoped>
 .modal-back {
-  width: 2000px;
-  height: 2000px;
+  cursor: default;
   position: fixed;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
   z-index: 999;
+  width: 100vw;
+  height: 100vh;
   top: 0%;
   left: 0%;
   background-color: rgba(38, 53, 48, 0.5);
+  font-family: "Outfit";
+  font-size: 18px;
+  gap: 15px;
+  color: #ffffff;
 }
 .modal-container {
-  font-size: 26px;
-  position: fixed;
-  z-index: 999;
-  top: 30%;
-  left: 50%;
-  margin-left: -200px;
-  border-radius: 12px;
-  min-width: 400px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: space-evenly;
+  gap: 15px;
+  flex-direction: column;
+  padding-top: 2em;
+  padding-bottom: 2em;
+  width: 15vw;
+  min-width: 200px;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+  height: fit-content;
 }
 </style>
