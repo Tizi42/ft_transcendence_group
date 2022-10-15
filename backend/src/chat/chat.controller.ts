c9f4584a2895f5706e384d8879ed89a82234ed15
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-10-15 18:04:56 +0200

    Revert "add type for lint"
    This reverts commit 72af3b7aafc0bbd359ce8406e9bf8ad72ba4efd2.

--- backend/src/chat/chat.controller.ts
+++ backend/src/chat/chat.controller.ts
@@ -31,8 +31,8 @@ export class ChatController {
     @Req() req: RequestWithUser,
     @Res() res: Response,
     @Param('id') id: number
-  ) : Promise<Chat[]> {
-    return await this.chatService.getMessagesById(id, req.user.id);
+  ) {
+    return res.json(await this.chatService.getMessagesById(id, req.user.id));
   }
 
   @Get('dest')
