4feb34963e1d056d0fcd5f30a2172a7e407dc03a
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-10-24 12:19:07 +0200

    missing files

---
+++ frontend/src/components/Game/ReadyButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="readyBtn">ReadyButton</div>
+</template>
+
+<script lang="ts" setup>
+import { defineComponent, defineExpose } from "vue";
+
+defineExpose(
+  defineComponent({
+    name: "ReadyButton",
+  })
+);
+</script>
+
+<style scoped>
+.readyBtn {
+  position: fixed;
+  top: 10%;
+  left: 10%;
+}
+</style>
