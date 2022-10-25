93430da70c5252178f9b5364130944e39e8cfe26
pbonilla <pbonilla@student.42.fr>
2022-10-25 12:20:11 +0200

    remove unnecessary files

--- frontend/src/game/sprites/racket.ts
+++
@@ -1,9 +0,0 @@
-export class Racket extends Phaser.Physics.Arcade.Sprite {
-  constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene, x, y, "paddle", 0);
-    scene.add.existing(this);
-    scene.physics.add.existing(this);
-    this.setCollideWorldBounds(true);
-    this.setImmovable(true);
-  }
-}
