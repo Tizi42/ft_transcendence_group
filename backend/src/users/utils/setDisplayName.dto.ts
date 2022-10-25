b4f273c2fb87bd99ffe08336247bf56df589a380
Tongyu YUAN <tyuan@student.42.fr>
2022-10-25 16:45:56 +0200

    fix 42 avatar & displayname submit event

--- backend/src/users/utils/setDisplayName.dto.ts
+++ backend/src/users/utils/setDisplayName.dto.ts
@@ -3,7 +3,6 @@ import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 export class SetDisplayNameDto {
     @IsNotEmpty()
     @IsString()
-    @MinLength(3)
     @MaxLength(16)
     displayname: string;
 }
