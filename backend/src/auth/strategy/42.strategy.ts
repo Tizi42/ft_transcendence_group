d133b91d7075776ed8677825fd6d7010815a0048
Lucie Le Briquer <lucie@lucie-lebriquer.fr>
2022-10-26 12:57:26 +0200

    need to fix done is not a function error

--- backend/src/auth/strategy/42.strategy.ts
+++ backend/src/auth/strategy/42.strategy.ts
@@ -3,7 +3,6 @@ import { PassportStrategy } from "@nestjs/passport";
 import { Profile } from "passport";
 import { Strategy } from "passport-42";
 import { VerifyCallback } from "passport-oauth2";
-import { User } from "src/users/users.entity";
 import { AuthService } from "../auth.service";
 
 @Injectable()
@@ -16,15 +15,16 @@ export class FortyTwoStrategy extends PassportStrategy(Strategy) {
             clientSecret: process.env.FORTYTWO_CLIENT_SECRET,
             callbackURL: "http://localhost:3000/api/auth/42/redirect",
             scope: ['public'],
+            passReqToCallback: true,
         });
     }
 
-    async validate(profile: Profile, done: VerifyCallback) {
+    async validate(accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) {
         const user = await this.authService.validateUser({
             username: profile.username,
             displayName: profile.displayName,
-            email: profile.emails[0].value,
-            picture: profile.photos[0].value,
+            email: profile.emails == undefined ? "" : profile.emails[0].value,
+            picture: profile.photos == undefined ? "" : profile.photos[0].value,
         });
         
         done(null, user);
